a7f2d8f864cad160f454a79ff0f39035b5875c88
Hager.Abdelghafar <Hager.Abdelghafar@ihorizons.com>
2022-06-17 09:54:33 +0200

    feat(initial commit) : add lint ,prettier,husky

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  quoteProps: "as-needed",
+  semi: true,
+  printWidth: 100,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+/**
+ * @flow
+ * Created by Hagar Abdelghafar on 04.06.2021
+ */
+
+import React, {FC} from 'react';
+import {View} from 'react-native';
+
+const App: FC = () => {
+  return <View style={{flex: 1,backgroundColor:'red'}} />;
+};
+
+export default App;
